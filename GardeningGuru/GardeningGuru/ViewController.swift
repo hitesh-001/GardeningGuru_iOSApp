fa276dc1a39ff0628d9f3ab83ef80cfad4245aeb
Hitesh Goyal <goyalhitesh371@gmail.com>
2024-04-25 23:55:41 +0530

    commit 2

--- GardeningGuru/GardeningGuru/ViewController.swift
+++ GardeningGuru/GardeningGuru/ViewController.swift
@@ -1,4 +1,4 @@
-//
+
 //  ViewController.swift
 //  GardeningGuru
 //
@@ -7,7 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return descriptionText.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! NotificationHomeCollectionViewCell
+        
+        cell.frontLabel.text = frontText[indexPath.row]
+        cell.descriptionLabel.text = descriptionText[indexPath.row]
+        
+        cell.layer.cornerRadius = 10.0
+        
+        return cell
+    }
+    
+    
+    var frontText: [String] = ["Watering Day :","Composting Day :","Litting Day :"]
+    
+    var descriptionText: [String] = ["Your Lemon plant need water todey","Your Lemon plant need compost todey","Your Lemon plant need litting todey"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
